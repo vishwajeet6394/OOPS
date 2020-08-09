20df47435fe237c528056cb6a7201923b759f351
vishwajeet6394 <68841430+vishwajeet6394@users.noreply.github.com>
2020-08-09 13:03:14 +0530

    update insert.cpp

--- ASSIGNMENT/ASSIGNMENT1/insert.cpp
+++ ASSIGNMENT/ASSIGNMENT1/insert.cpp
@@ -1,1 +1,26 @@
 
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int n,a[50];
+cin>>n;
+for(int i=0;i<n;i++){
+cin>>a[i];
+}
+for(int i=1;i<n;i++){
+int value=a[i];
+int   k=i;
+while(k>0&&a[k-1]>value){
+a[k]=a[k-1];
+k=k-1;
+}
+a[k]=value;
+}
+for(int i=0;i<n;i++){
+cout<<a[i]<<' ';}
+     return 0;
+
+}
