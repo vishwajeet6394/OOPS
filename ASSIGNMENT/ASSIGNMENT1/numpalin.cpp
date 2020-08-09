b06d81926c8027a72a2c553859cb5723e1f81d58
vishwajeet6394 <68841430+vishwajeet6394@users.noreply.github.com>
2020-08-09 13:05:30 +0530

    Update numpalin.cpp

--- ASSIGNMENT/ASSIGNMENT1/numpalin.cpp
+++ ASSIGNMENT/ASSIGNMENT1/numpalin.cpp
@@ -1,1 +1,17 @@
 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int n,sum=0;
+cin>>n;
+for(int i=n;i!=0;i=i/10){
+ sum=sum*10+i%10;
+}
+if(n==sum)
+    cout <<"palindrome";
+else
+cout<<"not palindrome";
+    return 0;
+}
