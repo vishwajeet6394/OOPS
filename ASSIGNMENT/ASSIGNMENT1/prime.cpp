1e1cb8e776fcef3ab365ddfcd8ca686c079f542a
vishwajeet6394 <68841430+vishwajeet6394@users.noreply.github.com>
2020-08-09 13:06:18 +0530

    Update prime.cpp

--- ASSIGNMENT/ASSIGNMENT1/prime.cpp
+++ ASSIGNMENT/ASSIGNMENT1/prime.cpp
@@ -1,1 +1,23 @@
 
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+int n;
+cin>>n;
+
+for(int i=2;i*i<=n;i++){
+if(n%i==0){
+cout<<"not prime";
+n=0;
+break;
+
+}
+}
+if(n){
+    cout<<"prime";
+  }
+    return 0;
+}
