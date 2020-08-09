69da4a7b623162fc10596a0cdd1d4f15fa88d108
vishwajeet6394 <68841430+vishwajeet6394@users.noreply.github.com>
2020-08-09 13:04:51 +0530

    Update maxima.cpp

--- ASSIGNMENT/ASSIGNMENT1/maxima.cpp
+++ ASSIGNMENT/ASSIGNMENT1/maxima.cpp
@@ -1,1 +1,25 @@
 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int n,a[50];
+cin>>n;
+for(int i=0;i<n;i++){
+cin>>a[i];
+}
+for(int i=0;i<n-1;i++){
+for(int j=i+1;j<n;j++){
+if(a[i]>a[j]){
+int temp=a[j];
+    a[j]=a[i];
+    a[i]=temp;
+}
+}
+}
+cout<<""<<a[1]<<" second minimum\n";
+cout<<""<<a[n-2]<<" second maximum\n";
+     return 0;
+
+}
