6fd00760a19800c2daad783cb5b23dab160c4ab4
vishwajeet6394 <68841430+vishwajeet6394@users.noreply.github.com>
2020-08-09 13:07:01 +0530

    Update stringpalin.cpp

--- ASSIGNMENT/ASSIGNMENT1/stringpalin.cpp
+++ ASSIGNMENT/ASSIGNMENT1/stringpalin.cpp
@@ -1,1 +1,26 @@
 
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+int main()
+{
+int f=1;
+char s[50];
+cin>>s;
+
+for(int i=0;i<strlen(s)/2;i++){
+if(s[i]!=s[strlen(s)-i-1]){
+cout<<"not";
+ f=0;
+break;
+}
+}
+
+if(f){
+    cout<<"palindrome";
+  }
+    return 0;
+}
