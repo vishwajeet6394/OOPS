413d1d660242abd67a97639c1ca62e8a6e7229aa
vishwajeet6394 <68841430+vishwajeet6394@users.noreply.github.com>
2020-08-09 13:01:44 +0530

    update greatest.cpp

--- ASSIGNMENT/ASSIGNMENT1/greatest.cpp
+++ ASSIGNMENT/ASSIGNMENT1/greatest.cpp
@@ -1,1 +1,18 @@
 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int a,b,c;
+cin>>a>>b>>c;
+if(a>b&&a>c){
+cout<<""<<a<<" is the greatest";
+}
+else if(b>a&&b>c)
+cout<<""<<b<<" is the greatest";
+else
+cout<<""<<c<<" is the greatest";
+    return 0;
+}
+
