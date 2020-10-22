cb72f2112d31c08987739808826ccbd071dfc77b
vishwajeet6394 <vishwajeetraj2001@gmail.com>
2020-10-22 15:04:33 +0530

    commit question 1 of assignment 4

---
+++ ass4question1.cpp
@@ -0,0 +1,17 @@
+Q. Discuss the ways using suitable examples in
+which inheritance promotes software reuse, saves
+time during program development and helps
+prevent errors.
+Ans: Inheritance allows developers to create
+subclasses that reuse code declared already in a
+superclass. Avoiding the duplication of common
+functionality between several classes by building a
+class inheritance hierarchy can save developers a
+considerable amount of time. Similarly, placing
+common functionality in a single superclass, rather
+than duplicating the code in multiple unrelated
+classes, helps prevent the same errors from
+appearing in multiple source-code files. If errors
+occur in the common functionality of the
+superclass, the software developer needs to modify
+only the superclass’s
